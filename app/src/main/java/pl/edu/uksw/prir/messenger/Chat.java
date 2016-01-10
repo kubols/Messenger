f2cd9fb6b8eddc72063b480676fd60cb1ac4873f
Jakub Pawlak <kubols@gmail.com>
2016-01-10 11:25:58 +0100

    Temporary working layout

--- app/src/main/java/pl/edu/uksw/prir/messenger/Chat.java
+++ app/src/main/java/pl/edu/uksw/prir/messenger/Chat.java
@@ -2,12 +2,27 @@ package pl.edu.uksw.prir.messenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
+
+    private TextView msgView;
+    private EditText msgEditText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        msgView = (TextView)findViewById(R.id.msgTextView);
+        msgEditText = (EditText) findViewById(R.id.msgEditText);
+    }
+
+    private void sendMessage(View view){
+        String msg = msgEditText.getText().toString();
     }
 }
