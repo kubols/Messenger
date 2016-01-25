dbf7b7e1aca79bf6990945fcf1486d297e236d06
Herzy <Herzy>
2016-01-25 15:55:49 +0100

    fixed receiving message

--- app/src/main/java/pl/edu/uksw/prir/messenger/MainActivity.java
+++ app/src/main/java/pl/edu/uksw/prir/messenger/MainActivity.java
@@ -13,10 +13,13 @@ package pl.edu.uksw.prir.messenger;
 
 import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.net.InetAddress;
@@ -48,15 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ACTION_NEW_MSG = "pl.edu.uksw.prir.messenger.NEW_MSG";
     public static final String MSG_FIELD = "message";
-    private BroadcastReceiver myReceiver;
 
 
-    public void onReceive(Context context, Intent intent) {
-        Bundle extras = intent.getExtras();
-        if (extras != null) {
-            }
-        Log.i("cos","przyszedl broadcast");
-        }
+
+    private IntentFilter filter = new IntentFilter("pl.edu.uksw.prir.messenger.Chat");
+
+
+
 
     public final static String TAG = "ActivityMain";
     public static final String PKG = "info.lamatricexiste.network";
@@ -67,18 +68,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chatButton = (Button) findViewById(R.id.chatButton);
-
+        //showToast("cos");
         Context context = getApplicationContext();
-
-
-
-
-
-
 
         Log.i("cos", "start serw");
         try {
-            EchoWorker worker = new EchoWorker();
+            EchoWorker worker = new EchoWorker(context);
             new Thread(worker).start();
             new Thread(new NioServer(InetAddress.getByName("192.168.2.103"), 9090, worker)).start();
             Log.i("cos", "start serw udany");
@@ -88,43 +83,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context, "start serw nieuadny", Toast.LENGTH_LONG).show();;
 
         }
-        new LongOperation().execute("");
-       // startService(new Intent(this, MessangerService.class));
+
+        // startService(new Intent(this, MessangerService.class));
 
     }
 
 
-    private class LongOperation extends AsyncTask<String, Void, String> {
 
-        Context context = getApplicationContext();
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                NioClient client = new NioClient(InetAddress.getByName("192.168.2.103"), 9090);
-                //NioClient client = new NioClient(InetAddress.getByName("www.google.com"), 80);
-
-                Thread t = new Thread(client);
-                t.setDaemon(true);
-                t.start();
-                RspHandler handler = new RspHandler();
-                client.send("maly krok dla czlowieka".getBytes(), handler);
-                handler.waitForResponse();
-                Log.i("cos", "start klient udany");
-            } catch (Exception e) {
-                e.printStackTrace();
-                Log.i("cos", "start klient nieudany");
-
-            }
-            Log.i("cos", "executed");
-            return "Executed";
-        }
-
-        @Override
-        protected void onPreExecute() {}
-
-        @Override
-        protected void onProgressUpdate(Void... values) {}
-    }
 
     public void openChat(View view){
         to = chatButton.getText().toString();
@@ -135,8 +100,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("to", to);
         startActivity(intent);
     }
-
-
 
 
 }
